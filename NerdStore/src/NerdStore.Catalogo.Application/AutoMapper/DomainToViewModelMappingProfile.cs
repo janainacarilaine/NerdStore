e545ceaf804273a2f60adec484a2b141f56fec24
Janaina Santos <65786694+janainacarilaine@users.noreply.github.com>
2022-02-11 13:16:26 -0300

    Criação da camada Application do contexto de catálogo

---
+++ NerdStore/src/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Altura, option => option.MapFrom(x => x.Dimensoes.Altura))
+                .ForMember(d => d.Largura, option => option.MapFrom(x => x.Dimensoes.Largura))
+                .ForMember(d => d.Profundidade, option => option.MapFrom(x => x.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
